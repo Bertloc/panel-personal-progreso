a6a411b1acc44119ac4d55c5058515da67b73c7b
Humberto Melquiades <humberto.melquiades@amentum.net>
2026-07-02 17:48:25 -0600

    fase 7-parte2

--- src/app/core/config/supabase.config.ts
+++ src/app/core/config/supabase.config.ts
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url: 'https://YOUR_PROJECT.supabase.co',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  url: 'https://tqflmagzhdajiqhcuodz.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZmxtYWd6aGRhamlxaGN1b2R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2OTUzOTgsImV4cCI6MjA5ODI3MTM5OH0.6ZyPD9V60yJbJ-ekHSvzvBb_vShXrgu5pmeIjOYlZJ8',
 };
